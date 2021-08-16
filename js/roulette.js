b453f600c747ecf871b538bcb48c7117d60f7797
Kazuyoshi Muramatsu <11132217+0712kazu@users.noreply.github.com>
2021-08-16 10:26:15 +0900

    Add files via upload

---
+++ js/roulette.js
@@ -0,0 +1,358 @@
+var mymap = L.map('mapid');
+
+const gsi_blank = new L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/blank/{z}/{x}/{y}.png', {
+  attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル白地図</a>",
+  opacity:0.9,
+  maxZoom: 7
+}).addTo(mymap);
+
+const gsi_map = new L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
+  attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル標準地図</a>",
+  opacity:0.7,
+  maxZoom: 7
+});
+
+const tcg500k = new L.tileLayer('http://alacarte.tcg.co.jp/tcg500k/WM_CT/tiles/{z}/{x}/{y}.png', {
+  attribution: "<a>TCG500k(2015)</a>",
+  opacity:0.7,
+  maxZoom: 10
+});
+// }).addTo(mymap);
+// L.control.zoomLabel().addTo(map);
+// mymap.setView([35.3622222, 138.7313889], 5);
+mymap.fitBounds([[45.30,126.50],[23.00,143.00]])
+
+const style = (feature) => {
+// const style = {
+  return {
+    stroke:false,
+    weight: 5,
+    fillOpacity: 0.8,
+    fillColor:'gray'
+    };
+}
+
+const highlightFeature = (e) => {//マウスホバーしたポリゴンに対して境界線の強調表示を行う
+  var layer = e.target;
+
+  layer.setStyle({//マウスホバーしたら太字の枠線をつける
+    stroke:true,
+    color: '#666',
+    // fillOpacity: 1,
+  });
+  if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {//ブラウザの設定？
+    layer.bringToFront();
+  }
+  // info.update(layer.feature.properties);
+}
+
+const resetHighlight = (e) => {
+  geojson.setStyle({
+    stroke:false,
+  });
+  // info.update();
+}
+const zoomToFeature = (e) => {// クリックした都道府県を全体表示してズーム
+  // mymap.fitBounds([[46.10,124.00],[23.00,153.00]])
+  geojson.setStyle({
+    fillOpacity:0.6,
+    stroke:false
+  })
+  e.target.setStyle({
+    fillOpacity:0.2,
+    stroke:true
+  })
+
+  window.setTimeout(()=> {
+    mymap.fitBounds([[e.target._bounds._northEast.lat,e.target._bounds._northEast.lng],[e.target._bounds._southWest.lat,e.target._bounds._southWest.lng]]);
+  },100)
+  // console.log(e.target)
+}
+
+
+const onEachFeature = (feature, layer) => {
+  layer.on({
+    mouseover: highlightFeature,//強調表示する
+    mouseout: resetHighlight,//強調表示を消す
+    click: zoomToFeature//クリックしたらズームして特定のポリゴンの強調表示
+  });
+  if(feature.properties && feature.properties.name){
+    layer.bindPopup(feature.properties.name);
+  }
+}
+
+let prefectureClick = null;
+
+geojson = L.geoJSON(prefecturesPoly, {
+  style: style,
+  onEachFeature: onEachFeature
+}).addTo(mymap);
+console.log(geojson)
+
+
+const Map_BaseLayer = {
+  '地理院地図': gsi_map,
+  '白地図': gsi_blank,
+  'TCG500k':tcg500k,
+};
+
+const Map_AddLayer = {
+  // '地理院地図': gsi_map,
+  // 'MIERUNE MONO': m_mono,
+};
+
+L.control.layers(Map_BaseLayer, Map_AddLayer,{
+  collapsed: false,
+}).addTo(mymap);
+
+L.control.zoomLabel({position: 'bottomleft'}).addTo(mymap);
+// L.control
+//   .opacity(Map_AddLayer, {
+//       label: 'Layers Opacity',
+//   })
+//   .addTo(map);
+
+const fitlayer = () => {//全体表示
+  const fitBtnEvent = document.getElementById('zoomstyle');
+  fitBtnEvent.addEventListener('click', () => {
+    mymap.fitBounds([[45.30,126.50],[23.00,143.00]])
+  })
+}
+fitlayer();
+
+class SelectClass {
+  constructor(){
+    this.cityList =[];
+    // this.selectNum = null;
+    this.roulette = null;
+    this.text = null;
+    this.jsonObj = null;
+    this.selectCity = null;
+  };
+
+  makeCityList (){//geojsonから都道府県名を取得してリスト化 20210712
+    this.prefNamesBefore = prefecturesPoly.features.map(feature => {
+      return feature.properties.name
+    })
+    this.cityList = this.prefNamesBefore.filter(function (x, i, self) {//重複を削除
+      return self.indexOf(x) === i;
+    });
+  };
+  
+  selectPrefectures () {
+    this.roulette = document.getElementById('roulette');
+    this.selectNum = Math.floor(Math.random() * 47)//都道府県コードをランダム
+    this.selectCity = String(this.cityList[this.selectNum]);//選ばれた都道府県名をリストから文字列で変数に代入
+    this.roulette.textContent = `${this.selectCity}`;//県名を画面に表示させる。
+    // console.log('文字表示１：'+this.selectCity)
+  };
+
+  viewLastPref(){
+    this.roulette = document.getElementById('roulette');
+    this.roulette.textContent = '答え： ' + `${this.selectCity}`;//県名を画面に表示させる。
+  }
+
+  clearGsiMap(){
+    // this.mapidStyle = document.getElementById('mapid');
+    // this.mapidStyle.style.opacity = 0.8
+  }
+  
+  changeColorSelectPoly () {//ランダムに都道府県を選んでその属性と同じポリゴンの色を変える。
+    this.selectPrefectures ()
+    geojson.eachLayer(layer => {
+      if(layer.feature.properties.name == this.selectCity){
+        // console.log('レイヤー：'+layer.feature.properties.name)
+        // console.log('文字表示２：'+this.selectCity)
+        layer.setStyle({
+          fillColor:'red',
+          fillOpacity:1
+          ,
+          stroke:false
+        })
+      }
+    }) 
+  }
+
+  
+  clearColorSelectPoly () {//つけた色を消す
+    geojson.setStyle({
+      stroke:false,
+      weight: 5,
+      fillOpacity: 0.6,
+      fillColor:'gray'
+    })
+  }
+
+  clearRouletteText(layer){
+    this.roulette.textContent = ``;//県名を一旦消す。
+    geojson.eachLayer(layer => {
+      if(layer.feature.properties.name == this.selectCity){
+        mymap.fitBounds([[layer._bounds._northEast.lat*0.99,layer._bounds._northEast.lng*1.01],[layer._bounds._southWest.lat*1.01,layer._bounds._southWest.lng*0.99]]);
+      }
+    }) 
+  }
+
+  countDown () {
+    this.totalTime = 5000;
+    this.oldTime = Date.now();
+    this.timeId = setInterval(() => {
+      this.currentTime = Date.now();
+      this.diff = this.currentTime - this.oldTime;// 差分を求める
+      this.diffSec = this.totalTime - this.diff;
+      this.remainSec = Math.ceil(this.diffSec / 1000);//ミリ秒を整数に変換
+      this.text = `${this.remainSec}`;
+      if (this.diffSec <= 0) {// 0秒以下になったら
+        clearInterval(this.timeId);
+        this.text = "";// タイマー終了
+      }
+      document.querySelector('#count_down').innerHTML = this.text;  // 画面に表示する
+    })
+  }
+}
+
+const selectCtyInstance = new SelectClass();
+selectCtyInstance.makeCityList();
+
+// const musicIntro = new Audio('audio/intro.mp3');
+const musicIntro = new Audio('audio/intro_part2.mp3');
+// const musicTypewriter = new Audio('audio/typewriter.mp3')
+const musicTypewriter = new Audio('audio/select_part2.mp3')
+// const musicCountDown =new Audio('audio/countdown.mp3')
+const musicCountDown =new Audio('audio/think_part2.mp3')
+
+class AudioClass {
+  constructor(){
+    this.speacker = document.getElementById('speaker_icon')
+    this.loop = null;
+    this.vl = 0.3
+    this.speacker.addEventListener('click', () => {
+      clearInterval(this.loop);
+      musicIntro.pause();
+      if (this.speacker.className === 'speaker_on'){
+        this.offSpeaker()//disabledがtrueでもfalse でも同じなのでは判定はなし
+      }else{
+        if(this.btn.classList.contains('disabled') == true){
+          this.onSpeaker();
+        }else {
+          this.onSpeaker();
+          this.playIntro();
+        }
+      }
+    });
+
+    this.btn = document.getElementById('btn_id')
+    this.btn.addEventListener('click', () => {//クリックしたらルーレットがはじまる。
+      this.btn.classList.add('disabled')
+      selectCtyInstance.clearGsiMap()
+      document.querySelector('#answer').innerHTML = ''
+      this.selectMusic();
+      this.interval = window.setInterval(() => {
+        selectCtyInstance.clearColorSelectPoly();
+        // selectCtyInstance.selectPrefectures();
+        selectCtyInstance.changeColorSelectPoly();
+      },80)
+    
+      window.setTimeout( () => {//カウントダウンが始まる
+        clearInterval(this.interval)
+        voiceOn();
+        selectCtyInstance.clearColorSelectPoly();
+        selectCtyInstance.changeColorSelectPoly();
+        selectCtyInstance.countDown();
+        selectCtyInstance.clearRouletteText();
+        
+        window.setTimeout(() => {//答えが出てもとに戻る
+          this.btn.classList.remove('disabled');
+          // selectCtyInstance.selectPrefectures();
+          selectCtyInstance.viewLastPref();
+          mymap.fitBounds([[45.30,126.50],[23.00,143.00]])
+          recognition.stop()
+          clearInterval(this.loop);
+          if (this.speacker.className === 'speaker_on'){
+            musicIntro.pause();
+            this.playIntro();
+          }
+        },5000);
+      }, 2000);
+    })
+  }
+  
+  offSpeaker () {//実はオフにしても裏でイントロは流れている。
+    this.speacker.classList.remove('speaker_on')
+    this.speacker.src ='img/icon_120980_256.png'
+    musicIntro.volume = 0;
+    this.typeCountVolZero();
+  }
+  
+  onSpeaker () {
+    this.speacker.classList.add('speaker_on')
+    this.speacker.src ='img/icon_120970_256.png'
+    this.typeCountVolUp();
+  }
+  
+  playIntro () {
+    this.introPlay();
+    musicIntro.volume = 0.1;
+    this.loop = window.setInterval(()=> {
+      musicIntro.pause();
+      this.introPlay();
+    // },36200)//intro.mp3の場合
+    },8740)//intro_part2.mp3の場合(BPM110:4小節)
+  }
+  
+  introPlay () {
+    musicIntro.currentTime = 0;
+    musicIntro.play();
+  }
+
+  typeCountVolZero () {
+    musicTypewriter.volume = 0;
+    musicCountDown.volume = 0;
+  }
+
+  typeCountVolUp () {
+    musicTypewriter.volume = this.vl;
+    musicCountDown.volume = this.vl;
+  }
+  
+  selectMusic () {
+    musicTypewriter.play();
+    musicIntro.volume = 0;
+    musicCountDown.pause();
+    musicCountDown.currentTime = 0;
+    if (this.speacker.className === 'speaker_on') {
+      this.typeCountVolUp();
+    }else{
+      this.typeCountVolZero();
+    }
+    window.setTimeout(()=> {
+      musicCountDown.play();
+      // console.log("countdown１")
+    },2000)
+  }
+}
+
+const audio = new AudioClass();
+
+//ここから音声認識
+SpeechRecognition = webkitSpeechRecognition || SpeechRecognition;
+if ('SpeechRecognition' in window) {
+  // ユーザのブラウザは音声合成に対応しています。
+} else {
+  // ユーザのブラウザは音声合成に対応していません。
+}
+const recognition = new SpeechRecognition();
+const voiceOn = () => {
+  recognition.onresult = (event) => {
+    // console.log(selectCtyInstance.cityList)
+    this.speechpref = selectCtyInstance.cityList.filter( pref => {
+      // console.log(pref)
+      return pref == event.results[0][0].transcript
+    });
+    // console.log(event)
+    if (speechpref == event.results[0][0].transcript){
+      document.querySelector('#answer').innerHTML = event.results[0][0].transcript
+    }
+  }
+  recognition.start();
+  // const answer = document.getElementById('answer')
+}
